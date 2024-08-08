b6c528844cbac6fb1f312962427825b285b38845
mvula <mvulakolweni08@gmail.com>
2024-08-08 23:06:05 +0200

    Added Navbar logo and options available to select (Uni / High)

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,19 @@ import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+        <div className='nav-container'>
+            <div className='nav-content'>
+            <h4 className='logo'>S2T</h4>
+
+                <ul>
+                    <li className='menu-item'>University</li>
+                    <li className='menu-item'>High School</li>
+                </ul>
+            </div>
+
+        </div>
+    </>
   )
 }
 
