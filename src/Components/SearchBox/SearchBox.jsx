0b1e2333fbca15e1d0100e20fc8ecbec24caea37
mvula <mvulakolweni08@gmail.com>
2024-08-09 01:42:15 +0200

    added more weights on the ggole font, created SearchBox component for the search-bar, and added heading text right under the nav-bar

---
+++ src/Components/SearchBox/SearchBox.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div>SearchBox</div>
+  )
+}
+
+export default SearchBox
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -6,7 +6,11 @@ import NavBar from '../../Components/NavBar/NavBar';
 const Landing = () => {
   return (
     <div>
-        <NavBar />
+        <div className='top-section'>
+          <NavBar />
+          <h1 className='usp-heading'>Share to Inspire</h1>
+          <p className='usp-text'>Your one-stop spot for top quality educational resources</p>
+        </div>
         
     </div>
   )
