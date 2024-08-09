17988e7a60db6ec87c4bfd50899d4051cc2f5bdb
mvula <mvulakolweni08@gmail.com>
2024-08-10 00:17:21 +0200

    created the hero section of the  landing page, created a services section right after the "top-section"

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -7,15 +7,57 @@ import SearchBox from '../../Components/SearchBox/SearchBox';
 const Landing = () => {
   return (
     <div>
-
+      {/* --------------------------------------------------------------------------- */}
         <div className='top-section'>
           <NavBar />
           <h1 className='usp-heading'>Share to Inspire</h1>
           <p className='usp-text'>Your one-stop spot for highend quality education resources</p>
           <SearchBox />
 
-          <h2 className='landing-down-arrow'> &#8964; </h2>
-          
+          <h2 className='landing-down-arrow'> &#8964; </h2>  
+        </div>
+        {/* ------------------------------------------------------------------------- */}
+        <div className='landing-info-panel'>
+          {/* Panel 3 */}
+          <div className='info-panel'>
+            <img src="../../../assets/dog.svg" alt="" />
+            <h4 className='info-panel-heading'>Heading 1</h4>
+            <p className='info-panel-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.</p>
+          </div>
+          {/* Panel 3 */}
+          <div className='info-panel'>
+            <img src="../../../assets/dog.svg" alt="" />
+            <h4 className='info-panel-heading'>Heading 1</h4>
+            <p className='info-panel-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.</p>
+          </div>
+            {/* Panel 3 */}
+          <div className='info-panel'>
+            <img src="../../../assets/dog.svg" alt="" />
+            <h4 className='info-panel-heading'>Heading 1</h4>
+            <p className='info-panel-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.</p>
+          </div>
+        </div>
+
+        {/* ------------------------------------------------------------------------ */}
+
+        <div className='landing-hero-section'>
+
+          <div className='hero-section'>
+            <img src="../../../public/assets/dog.svg" alt="" />
+              <div>
+                <h4>Heading</h4>
+                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem vel laborum exercitationem quos. Placeat eum, tempore corporis illum repellendus modi, eius reprehenderit quibusdam tempora vel, natus labore non sed vero!</p>
+              </div>
+          </div>
+
+          <div className='hero-section'>
+            <div>
+              <h4>Heading</h4>
+              <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem vel laborum exercitationem quos. Placeat eum, tempore corporis illum repellendus modi, eius reprehenderit quibusdam tempora vel, natus labore non sed vero!</p>
+            </div>
+            <img src="../../../public/assets/dog.svg" alt="" />
+          </div>
+
         </div>
         
     </div>
