bc4e43312bc0fd3316303df5bb8f2a2aee5ce801
mvula <mvulakolweni08@gmail.com>
2024-08-20 05:06:03 +0200

    Added an info-panel component in the landing page to make it more generic when admin wants to change some information on the landing page

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,118 +1,143 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 import "./Landing.css";
-import NavBar from '../../Components/NavBar/NavBar';
-import SearchBox from '../../Components/SearchBox/SearchBox';
+import { INFO } from "../../Utilities/Landing-data";
+import NavBar from "../../Components/NavBar/NavBar";
+import SearchBox from "../../Components/SearchBox/SearchBox";
+import InfoPanel from "./InfoPanel/InfoPanel";
 
 const Landing = () => {
   return (
     <div>
       {/* --------------------------------------------------------------------------- */}
       <NavBar />
-        <div className='landing-top-section'>
-          <h1 className='usp-heading'>Share to Inspire</h1>
-          <p className='usp-text'>Your one-stop spot for highend quality education resources</p>
-          <SearchBox />
+      <div className="landing-top-section">
+        <h1 className="usp-heading">Share to Inspire</h1>
+        <p className="usp-text">
+          Your one-stop spot for highend quality education resources
+        </p>
+        <SearchBox />
+      </div>
+
+      {/* ------------------------------------------------------------------------- */}
+
+      <div className="landing-mid-section">
+        <div className="landing-info-panel">
+          {/* Panel 3 */}
+          {INFO.map((item) => (
+            <InfoPanel
+              key={item.title}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
         </div>
-        {/* ------------------------------------------------------------------------- */}
-        <div className='landing-mid-section'>
-            <div className='landing-info-panel'>
-              {/* Panel 3 */}
-              <div className='info-panel'>
-                <img src="../../../assets/dog.svg" alt="" />
-                <h4 className='info-panel-heading'>Heading 1</h4>
-                <p className='info-panel-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.</p>
-              </div>
-              {/* Panel 3 */}
-              <div className='info-panel'>
-                <img src="../../../assets/dog.svg" alt="" />
-                <h4 className='info-panel-heading'>Heading 1</h4>
-                <p className='info-panel-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.</p>
-              </div>
-                {/* Panel 3 */}
-              <div className='info-panel'>
-                <img src="../../../assets/dog.svg" alt="" />
-                <h4 className='info-panel-heading'>Heading 1</h4>
-                <p className='info-panel-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.</p>
-              </div>
-            
+
+        {/* ------------------------------------------------------------------------ */}
+
+        <div className="landing-hero-section">
+          <div className="hero-section">
+            <img src="../../../assets/dog.svg" alt="" />
+            <div>
+              <h4>Heading</h4>
+              <p>
+                {" "}
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Dolorem vel laborum exercitationem quos. Placeat eum, tempore
+                corporis illum repellendus modi, eius reprehenderit quibusdam
+                tempora vel, natus labore non sed vero!
+              </p>
             </div>
+          </div>
 
-            {/* ------------------------------------------------------------------------ */}
-
-            <div className='landing-hero-section'>
-              <div className='hero-section'>
-                <img src="../../../assets/dog.svg" alt="" />
-                  <div>
-                    <h4>Heading</h4>
-                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem vel laborum exercitationem quos. Placeat eum, tempore corporis illum repellendus modi, eius reprehenderit quibusdam tempora vel, natus labore non sed vero!</p>
-                  </div>
-              </div>
-
-              <div className='hero-section'>
-                <div>
-                  <h4>Heading</h4>
-                  <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem vel laborum exercitationem quos. Placeat eum, tempore corporis illum repellendus modi, eius reprehenderit quibusdam tempora vel, natus labore non sed vero!</p>
-                </div>
-                <img src="../../../assets/dog.svg" alt="" />
-              </div>
+          <div className="hero-section">
+            <div>
+              <h4>Heading</h4>
+              <p>
+                {" "}
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Dolorem vel laborum exercitationem quos. Placeat eum, tempore
+                corporis illum repellendus modi, eius reprehenderit quibusdam
+                tempora vel, natus labore non sed vero!
+              </p>
             </div>
-        </div>
-
-        <div className='landing-bottom-section'>
-          <div className='landing-teacher-info'>
-            <h1> Are you an Educator?</h1>
-            <p>Join a commnunity of educators who share innovative ideas  and  revolutionize your classes</p>
+            <img src="../../../assets/dog.svg" alt="" />
           </div>
         </div>
+      </div>
 
-        <div className='landing-footer-info'>
+      <div className="landing-bottom-section">
+        <div className="landing-teacher-info">
+          <h1> Are you an Educator?</h1>
+          <p>
+            Join a commnunity of educators who share innovative ideas and
+            revolutionize your classes
+          </p>
+        </div>
+      </div>
 
-          <div className='footer-info'>
-            <h4>Logo</h4>
-              
-            <button>login</button>
-          </div>
+      <div className="landing-footer-info">
+        <div className="footer-info">
+          <h4>Logo</h4>
 
-          <div className='footer-info'>
-            <h4>For Educators</h4>
-
-            <ul>
-              <a href="#"><li className='footer-item'>learn more</li></a>
-              <a href="#"><li  className='footer-item'>apply</li></a>
-            </ul>
-
-          </div>
-
-          <div className='footer-info'>
-            <h4>Platform</h4>
-
-            <ul>
-              <a href="#"><li  className='footer-item'>about us</li></a>
-              <a href="#"><li  className='footer-item'>contact us</li></a>
-              <a href="#"><li  className='footer-item'>self-directed learning</li></a>
-              <a href="#"><li  className='footer-item'>FAQ</li></a>
-            </ul>
-
-          </div>
-
-          <div className='footer-info'>
-            <h4>Legal</h4>
-
-            <ul>
-              <a href="#"><li  className='footer-item'>terms</li></a>
-              <a href="#"><li  className='footer-item'>privacy policy</li></a>
-              <a href="#"><li  className='footer-item'>disclaimer</li></a>
-            </ul>
-
-          </div>
+          <button>login</button>
         </div>
 
-        <div className='copyright-section'>
-          <h4>&copy; 2024 Share2Teach </h4>
-        </div>
-    </div>  
-  )
-}
+        <div className="footer-info">
+          <h4>For Educators</h4>
 
-export default Landing
+          <ul>
+            <a href="#">
+              <li className="footer-item">learn more</li>
+            </a>
+            <a href="#">
+              <li className="footer-item">apply</li>
+            </a>
+          </ul>
+        </div>
+
+        <div className="footer-info">
+          <h4>Platform</h4>
+
+          <ul>
+            <a href="#">
+              <li className="footer-item">about us</li>
+            </a>
+            <a href="#">
+              <li className="footer-item">contact us</li>
+            </a>
+            <a href="#">
+              <li className="footer-item">self-directed learning</li>
+            </a>
+            <a href="#">
+              <li className="footer-item">FAQ</li>
+            </a>
+          </ul>
+        </div>
+
+        <div className="footer-info">
+          <h4>Legal</h4>
+
+          <ul>
+            <a href="#">
+              <li className="footer-item">terms</li>
+            </a>
+            <a href="#">
+              <li className="footer-item">privacy policy</li>
+            </a>
+            <a href="#">
+              <li className="footer-item">disclaimer</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+
+      <div className="copyright-section">
+        <h4>&copy; 2024 Share2Teach </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ src/Utilities/Landing-data.js
@@ -0,0 +1,22 @@
+export const INFO = [
+  {
+    image: "../../assets/dog.svg",
+    title: "Heading 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.",
+  },
+
+  {
+    image: "../../assets/dog.svg",
+    title: "Heading 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.",
+  },
+
+  {
+    image: "../../assets/dog.svg",
+    title: "Heading 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem in eius ratione dolore, magnam reiciendis pariatur numquam culpa minima consectetur harum. Nostrum excepturi accusantium, quidem eligendi delectus cum? Mollitia, architecto.",
+  },
+];
