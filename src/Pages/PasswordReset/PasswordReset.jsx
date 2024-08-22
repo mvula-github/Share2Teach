30c837b543b2129e34ba7ccd8e00f8f62b8b4dae
Jameela-github <jameelasparks3@gmail.com>
2024-08-22 18:46:14 +0200

    Made a password reset folder and completed the .jsx file for password reset

---
+++ src/Pages/PasswordReset/PasswordReset.jsx
@@ -0,0 +1,27 @@
+//PasswordReset Page
+import React from 'react';
+import './PasswordReset.css';
+
+const PasswordReset = () => {
+    return (
+        <div>
+            <div className = ' container'>
+                <div className = 'header'>
+                    <div className='text'> Reset Password</div>
+                    <div className = 'underline'></div>                
+                </div>
+                <div className='inputs'>
+                    <div className='input'>
+                        <img src ='../../../assets/icons/emailAC.jpeg' alt = "description"/>
+                        <input type = "email" placeholder = "Enter your email" />
+                    </div>
+                </div>
+                <div className='submit-container'>
+                    <div className='submit'>Submit</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PasswordReset;
