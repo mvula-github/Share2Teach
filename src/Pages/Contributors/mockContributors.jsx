8762c4f2e8abbce9a9e8515286e7e39b9bebdbcf
NICK-TSHEPO <143218308+NICK-TSHEPO@users.noreply.github.com>
2024-08-24 22:32:58 +0200

    Added contributors mock data for testing.

---
+++ src/Pages/Contributors/mockContributors.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const mockContributors = {
+    founders: ['Dr. Chantelle Bosch', 'Mr. Johan Venter'],
+    administrators: ['Mr. John Klerck', 'Mr. Heiki Pelser'],
+    moderators: ['Prof. Dorothy Laubscher'],
+    educators: ['Mr. Heystek Grobler'],
+    resources: ['Mr. Nick Mogotlane', 'Mr. Mvula Kolweni', 'Miss Jameela Sparks', 'Miss Olwethu Zungu'],
+
+  };
+
+export default MockContributors;
+
+
+
+  
+  
