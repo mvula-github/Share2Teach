b1c93f147883abda1e74f900ce16f133a544e49c
Jameela-github <jameelasparks3@gmail.com>
2024-10-13 03:05:31 +0200

    added the about us page oath to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Landing from './Pages/Landing/Landing';
 import SignUp from './Pages/SignUp/SignUp';
 import PasswordReset from './Pages/PasswordReset/PasswordReset';
 import OER from './Pages/OER/OER';
+import AboutUs from './Pages/AboutUs/AboutUs'; // Step 1: Import the About Us component
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         } />
         <Route path="/reset-password" element={<PasswordReset />} />
         <Route path="/oer" element={<OER />} /> {/* Add route for OER */}
+        <Route path="/about-us" element={<AboutUs />} /> {/* Step 3: Add route for About Us */}
       </Routes>
     </Router>
   );
