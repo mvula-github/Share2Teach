a7a3ee6c79be5f801559b43440b96801091ddaa0
olwethuu <142734269+olwethuu@users.noreply.github.com>
2024-10-14 10:28:20 +0200

    added the NoPage for in the case of not finding any page, and also installed routers, but still fixing them

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import './App.css'
 import Landing from './Pages/Landing/Landing'
-
-
-
-
+// importing routers
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SelfDirect from "./Pages/Landing/SelfDirect"
+import Subjects from "./Pages/Landing/Subjects"
+// page for in the case of any errors
+import NoPage from './Pages/Landing/NoPage'
 
 function App() {
 
   return (
     <>
       <div className='app-container'>
-        <Landing />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path = "/" element = {<Landing />} />
+            <Route path = "/Landing" element = {<Landing/>} />
+            <Route path = "/SelfDirect" element = {<SelfDirect/>} />
+            <Route path = "/Subjects" element = {<Subjects/>} />
+            <Route path = "*" element = {<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+        
+
 
       </div>
     </>
---
+++ src/Pages/Landing/NoPage.jsx
@@ -0,0 +1,12 @@
+//import React from 'react';
+import "./NoPage.css";
+
+const NoPage = () => {
+    return(
+        <div className = 'no-page-container'>
+            <h1>Error 404: Not found :( </h1>
+        </div>
+    );
+};
+
+export default NoPage;
