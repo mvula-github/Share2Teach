6109b87d187b4a754064a53f58bf2d7066bb3a79
mvula <mvulakolweni08@gmail.com>
2024-10-19 15:17:43 +0200

    modified manageFAQ object assignment

--- src/Pages/AdminPage/FAQ/FAQ.jsx
+++ src/Pages/AdminPage/FAQ/FAQ.jsx
@@ -47,9 +47,9 @@ const FAQ = () => {
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.category}
-                name="Category"
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
+                name="category"
+                error={!!touched.category && !!errors.category}
+                helperText={touched.category && errors.category}
                 sx={{ gridColumn: "span 2" }}
               />
               <TextField
@@ -60,9 +60,9 @@ const FAQ = () => {
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.question}
-                name="Question"
-                error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && errors.lastName}
+                name="question"
+                error={!!touched.question && !!errors.question}
+                helperText={touched.question && errors.question}
                 sx={{ gridColumn: "span 3" }}
               />
               <TextField
@@ -75,9 +75,9 @@ const FAQ = () => {
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.answer}
-                name="Answer"
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
+                name="answer"
+                error={!!touched.question && !!errors.question}
+                helperText={touched.question && errors.question}
                 sx={{ gridColumn: "span 5" }}
               />
             </Box>
@@ -94,19 +94,14 @@ const FAQ = () => {
 };
 
 const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
+  category: yup.string().required("required"),
+  question: yup.string().required("required"),
+  answer: yup.string().required("required"),
 });
 const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  address1: "",
-  address2: "",
+  category: "",
+  question: "",
+  answer: "",
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
